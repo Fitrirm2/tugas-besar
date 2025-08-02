179c9c27c7f9b1e8a5dbf4d1966253252e119c82
Fitri <ramadhanifitrie223@gmail.com>
2025-08-02 13:42:21 +0700

    Update route.ts

--- app/api/modul/[id]/route.ts
+++ app/api/modul/[id]/route.ts
@@ -1,11 +1,11 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function PUT(
-  req: NextRequest,
+  req: Request,
   context: { params: { id: string } }
 ) {
-  const { id } = context.params; 
+  const { id } = context.params;
   const data = await req.json();
 
   try {
@@ -24,7 +24,7 @@ export async function PUT(
 }
 
 export async function DELETE(
-  req: NextRequest,
+  req: Request,
   context: { params: { id: string } }
 ) {
   const { id } = context.params;
